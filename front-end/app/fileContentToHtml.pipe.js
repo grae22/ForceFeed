8c86d77ba1b643ac6e04aa1eb17fa64542f2f961
GraemeB <grae22@gmail.com>
2016-06-10 08:24:23 +0200

    Some linting and tweaking of files.

--- front-end/app/fileContentToHtml.pipe.js
+++ front-end/app/fileContentToHtml.pipe.js
@@ -31,13 +31,21 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                     if (content.indexOf('===') === 0) {
                         isDiff = true;
                     }
+                    // Replace some non html safe chars. 
                     content = content.replace(new RegExp('\r', 'g'), '');
                     content = content.replace(new RegExp('<', 'g'), '&lt;');
                     content = content.replace(new RegExp('>', 'g'), '&gt;');
-                    content = content.replace(new RegExp('\t', 'g'), '<b><font color="#ff0000">[TAB]</font></b>');
-                    var newContent = '';
+                    content = content.replace(new RegExp('\t', 'g'), '<b><font color="#00ffff" style="background-color: #ff0000;">~tab~</font></b>');
+                    // Split up the content into lines.
                     var lines = content.split('\n');
+                    // Exclude the last line - it's always blank.
+                    if (lines.length > 1) {
+                        lines = lines.slice(0, lines.length - 1);
+                    }
+                    // Iterate through lines and format.
+                    var newContent = '';
                     var isFirstLine = true;
+                    var isPrevLineBlank = false;
                     for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
                         var line = lines_1[_i];
                         // Skip the first line - it's a header from p4 we don't want to see.
@@ -52,20 +60,43 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                         // Process the line.
                         var linePostfix = '';
                         if (line[0] == '-') {
-                            newContent += '<font color="#ff0000">';
-                            linePostfix += '</font>';
+                            newContent += '<span style="background-color: #ffc0c0;">';
+                            linePostfix += '</span>';
                         }
                         else if (line[0] == '+') {
-                            newContent += '<font color="#00a020">';
-                            linePostfix += '</font>';
+                            newContent += '<span style="background-color: #a0f0a0;">';
+                            linePostfix += '</span>';
                         }
+                        // Trailing whitespace.
+                        var trailingWhitespaceStartIndex = -1;
+                        var trailingWhitespaceCount = 0;
+                        for (var i = line.length - 1; i > -1; i--) {
+                            if (line[i] == ' ') {
+                                trailingWhitespaceStartIndex = i;
+                                trailingWhitespaceCount++;
+                            }
+                            else {
+                                break;
+                            }
+                        }
+                        if (trailingWhitespaceCount < line.length - 1 &&
+                            trailingWhitespaceCount > 0) {
+                            line = line.substr(0, trailingWhitespaceStartIndex);
+                            line += '<font color="#00ffff" style="background-color: #ff0000;"><b>';
+                            for (var i = 0; i < trailingWhitespaceCount; i++) {
+                                line += '~';
+                            }
+                            line += '</b></font>';
+                        }
+                        // Consecutive blank lines.
+                        var isLineBlank = (line.trim().length == 0);
+                        if (isLineBlank &&
+                            isPrevLineBlank) {
+                            line += '<font color="#00ffff" style="background-color: #ff0000;">~consecutive blank line~</font>';
+                        }
+                        isPrevLineBlank = isLineBlank;
                         // Add this line to the new content.
-                        if (isDiff) {
-                            newContent += line.substr(1, line.length - 1) + linePostfix + '\n';
-                        }
-                        else {
-                            newContent += line + linePostfix + '\n';
-                        }
+                        newContent += line + linePostfix + '\n';
                     }
                     return '<pre>' + newContent + '</pre>';
                 };
--- front-end/app/fileContentToHtml.pipe.ts
+++ front-end/app/fileContentToHtml.pipe.ts
@@ -23,14 +23,25 @@ export class FileContentToHtmlPipe
       isDiff = true;
     }
 
+    // Replace some non html safe chars. 
     content = content.replace( new RegExp( '\r', 'g' ), '' );
     content = content.replace( new RegExp( '<', 'g' ), '&lt;' );
     content = content.replace( new RegExp( '>', 'g' ), '&gt;' );
-    content = content.replace( new RegExp( '\t', 'g' ), '<b><font color="#ff0000">[TAB]</font></b>' );
+    content = content.replace( new RegExp( '\t', 'g' ), '<b><font color="#00ffff" style="background-color: #ff0000;">~tab~</font></b>' );
 
-    var newContent = '';
+    // Split up the content into lines.
     var lines = content.split( '\n' );
+
+    // Exclude the last line - it's always blank.
+    if( lines.length > 1 )
+    {
+      lines = lines.slice( 0, lines.length - 1 );
+    }
+
+    // Iterate through lines and format.
+    var newContent = '';
     var isFirstLine = true;
+    var isPrevLineBlank = false;
 
     for( var line of lines )
     {
@@ -50,26 +61,61 @@ export class FileContentToHtmlPipe
       // Process the line.
       let linePostfix: string = '';
 
-      if( line[ 0 ] == '-' )
+      if( line[ 0 ] == '-' )    // Subtractions.
       {
-        newContent += '<font color="#ff0000">';
-        linePostfix += '</font>';
+        newContent += '<span style="background-color: #ffc0c0;">';
+        linePostfix += '</span>';
       }
-      else if( line[ 0 ] == '+' )
+      else if( line[ 0 ] == '+' )   // Additions.
       {
-        newContent += '<font color="#00a020">';
-        linePostfix += '</font>';
+        newContent += '<span style="background-color: #a0f0a0;">';
+        linePostfix += '</span>';
       }
 
+      // Trailing whitespace.
+      var trailingWhitespaceStartIndex = -1;
+      var trailingWhitespaceCount = 0;
+
+      for( var i = line.length - 1; i > -1; i-- )
+      {
+        if( line[ i ] == ' ' )
+        {
+          trailingWhitespaceStartIndex = i;
+          trailingWhitespaceCount++;
+        }
+        else
+        {
+          break;
+        }
+      }
+
+      if( trailingWhitespaceCount < line.length - 1 &&    // Ignore white-space on empty lines.
+          trailingWhitespaceCount > 0 )
+      {
+        line = line.substr( 0, trailingWhitespaceStartIndex );
+        line += '<font color="#00ffff" style="background-color: #ff0000;"><b>';
+
+        for( var i = 0; i < trailingWhitespaceCount; i++ )
+        {
+          line += '~';
+        }
+
+        line += '</b></font>';
+      }
+
+      // Consecutive blank lines.
+      var isLineBlank = ( line.trim().length == 0 );
+
+      if( isLineBlank &&
+          isPrevLineBlank )
+      {
+        line += '<font color="#00ffff" style="background-color: #ff0000;">~consecutive blank line~</font>';
+      }
+
+      isPrevLineBlank = isLineBlank;
+
       // Add this line to the new content.
-      if( isDiff )
-      {
-        newContent += line.substr( 1, line.length - 1 ) + linePostfix + '\n';
-      }
-      else
-      {
-        newContent += line + linePostfix + '\n';
-      }
+      newContent += line + linePostfix + '\n';
     } 
     
     return '<pre>' + newContent + '</pre>';
